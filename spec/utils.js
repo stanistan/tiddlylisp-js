aa5598c47770cf1660ed47716f083085cbfc8b05
Stan Rozenraukh <stan@stanistan.com>
2012-10-13 12:33:19 -0400

    fix for travis

--- spec/utils.js
+++
@@ -1,9 +0,0 @@
-var _ = require('./../lib/utils.js');
-
-describe('zipmap', function() {
-
-  it('should work', function() {
-    expect(_.zip(['a', 'b', 'c'], [1, 2, 3])).tobe({a: 1, b: 2, c: 3})
-  });
-
-});
