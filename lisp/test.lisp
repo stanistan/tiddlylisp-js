17cf7847e82073561afcc93f880a490c5d241884
Stan Rozenraukh <stan@stanistan.com>
2012-10-15 17:47:22 -0400

    to new branch

--- lisp/test.lisp
+++
@@ -1,9 +0,0 @@
-(run 1000
-  (begin
-    (define t
-      (lambda (x)
-        (begin
-          (print x)
-          (current-counter-state)
-          (if (= 10 x) x (recur (inc x))))))
-    (t 1)))
