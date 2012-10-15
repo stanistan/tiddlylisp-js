eb0f8841d724e3ebfb500503b0727eeec9c19df8
Stan Rozenraukh <stan@stanistan.com>
2012-10-15 15:17:06 -0400

    (run)

--- lisp/test.lisp
+++ lisp/test.lisp
@@ -1,12 +1,9 @@
-(define t
-  (lambda (x)
-    (begin
-      (print x)
-      (current-counter-state)
-      (if (= 10 x) x (recur (inc x))))))
-
-(run 1 (t 1))
-(run 2 (t 1))
-(run 3 (t 1))
-
-
+(run 1000
+  (begin
+    (define t
+      (lambda (x)
+        (begin
+          (print x)
+          (current-counter-state)
+          (if (= 10 x) x (recur (inc x))))))
+    (t 1)))
