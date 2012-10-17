de3ad62057f3ac1b26a104fa189d5c89436cbc24
Stan Rozenraukh <stan@stanistan.com>
2012-10-17 16:23:50 -0400

    added properties

--- spec/envSpec.js
+++ spec/envSpec.js
@@ -43,3 +43,22 @@ describe('inner env', function() {
     }
   });
 });
+
+describe('using metadata [put|get]', function() {
+  var e = envs.initGlobal();
+  it('is settable', function() {
+    e.setProp('a', 'b', 1);
+    e.setProp('a', 'c', 2);
+    expect(e.getProp('a', 'b')).toEqual(1);
+    expect(e.getProp('a', 'c')).toEqual(2);
+  });
+  var e2 = envs.init([],[], e);
+  it('is nestable', function() {
+    expect(e2.getProp('a', 'b')).toEqual(e.getProp('a', 'b'));
+  });
+  it('returns null for things not found', function() {
+    expect(e.getProp('a', 'a')).toBe(null);
+    expect(e.getProp('b', 'c')).toBe(null);
+    expect(e2.getProp('asdfadf', 'sdfasdf')).toBe(null);
+  });
+});
