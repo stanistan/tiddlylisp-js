6cc25336b2f9deaeaabfe1abd43887aea602520d
Stan Rozenraukh <stan@stanistan.com>
2012-10-18 15:29:37 -0400

    run w/o repl with flag -r

--- main.js
+++ main.js
@@ -4,14 +4,24 @@ var lisp = require('./lib/parser.js')
   , file = require('./lib/file.js')
   , repl = require('./lib/repl.js');
 
-var parser = new lisp.Parser();
+var parser = new lisp.Parser()
+  , args = process.argv.slice(2)
+  , files = []
+  , flags = [];
+
+args.forEach(function(a) {
+  (a.indexOf('-') === 0 ? flags : files).push(a);
+});
+
 
 try {
   // read core
   file.read('./lisp/core.lisp', parser, function() {
 
-    file.load(process.argv.slice(2), parser, function() {
-      repl.init('=> ', parser);
+    file.load(files, parser, function() {
+      if (flags.indexOf('-r') < 0) {
+        repl.init('=> ', parser);
+      }
     });
 
   });
